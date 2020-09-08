23c019fc778f0af808b8d9783ed50f561fe4ce93
Konstantinos Nikoloutsos <konstantinosnikolouts@gmail.com>
2020-09-08 20:48:41 +0300

    Show json received for debugging

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -1,23 +1,29 @@
 #include <iostream>
+#include "Rand.h"
 #include "SparseMatrixTraits.h"
+#include <string>
+#include <cstdio>
+
+
 
 template <typename T>
 class Cell{
-    T data;
-    Coordinates coordinate;
+
 public:
     Cell(const Coordinates &coordinate) : coordinate(coordinate){}
 
     Cell() {
         std::cout << "test";
     }
+    Coordinates coordinate;
+    T data;
 };
-
 
 template <typename T>
 class SparseMatrix : public SparseMatrixTraits<T>{
 public:
     Cell<T> * * table;
+    inline static int itemsNumber = 0;
 
     SparseMatrix(){
         table = new Cell<T>*[10000000];
@@ -25,21 +31,34 @@ public:
 
     const T &operator()(Coordinates pos) const override {
         Cell<T> * b = new Cell<T>(pos);
+        table[SparseMatrix::itemsNumber] = b;
+        SparseMatrix::itemsNumber++;
         T * a = new T(1);
+        b->data = *a;
         return *a;
     }
 
     T &operator()(Coordinates pos) override {
+        for (int i = 0; i < SparseMatrix::itemsNumber; ++i) {
+            if(pos.col == table[i]->coordinate.col && pos.row == table[i]->coordinate.row){
+                return table[i]->data;
+            }
+        }
+
         Cell<T> * b = new Cell<T>(pos);
-        T * a = new T(1);
+        *(table + SparseMatrix::itemsNumber) = b;
+        std::cout<< "===" << SparseMatrix::itemsNumber << "=====\n";
+        SparseMatrix::itemsNumber = SparseMatrix::itemsNumber + 1;
+        T * a = new T(0);
+        b->data = *a;
         return *a;
     }
 
     void clear() override {
-
+        delete[] table;
     }
 
     void print() override {
-
+        std::cout << "This is a sparse table.";
     }
 };
